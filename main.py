be32d14b4ee13ac6954719a7bd0aaaf5b4575ed7
nk <nk.kozlww@gmail.com>
2022-09-16 15:28:05 +0500

    Вычисления дохода по вкладу

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-345
+print("Вычисления дохода по вкладу.")
+print("Введите исходные данные:")
+a=float(input("Величина вклада (руб.) -> "))
+b=float(input("Срок вклада (дней)-> "))
+c=float(input("Процентная ставка (годовых)-> "))
+print("----------------------------")
+d=(a/100*((b*c)/365))
+print("Доход: ","{:0.2f}".format(d))
+d=d+a
+print("Сумма по окончании срока вклада: ","{:0.2f}".format(d))
